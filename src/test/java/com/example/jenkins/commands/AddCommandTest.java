c36bde0ddfd9803753c5c71181bda5a53e19e919
thecl0ver <sirotkin.dmitry.v@gmail.com>
2021-09-12 22:40:49 +0300

    test

---
+++ src/test/java/com/example/jenkins/commands/AddCommandTest.java
@@ -0,0 +1,12 @@
+package com.example.jenkins.commands;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddCommandTest {
+
+    @Test
+    void execute() {
+    }
+}
